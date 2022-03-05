da351cffedf2bed4d225519f11e7e2b68e806748
Hüseyin Türeoğlu <huseyintureoglu@gmail.com>
2022-03-06 00:19:06 +0300

    first commit

---
+++ MukemmelSayiMi/Methodlarim.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MukemmelSayiMi
+{
+   public class Methodlarim
+    {
+        public int PozitifBolenlertoplami(int sayi)
+        {
+            int toplam = 0;
+            for (int i = 1; i <= sayi; i++)
+            {
+                if (sayi%i==0)
+                {
+                    toplam = toplam + i;
+                   // Console.WriteLine(i);
+                }
+            }
+            return toplam;
+        }
+        public bool MukemmelSayiMi(int sayi)
+        {
+            bool sonuc = false;
+            int toplam= PozitifBolenlertoplami(sayi);
+            if (sayi*2==toplam)
+            {
+                //Console.WriteLine(sayi + " mükemmel sayıdır");
+                sonuc = true;
+            }
+            else
+            {
+               // Console.WriteLine("Mükemmel sayı değildir.");
+            }
+            return sonuc;
+        }
+        public void AraliktakiMukemmelSayilariBul(int baslangic, int bitis)
+        {
+            for (int i = baslangic; i < bitis; i++)
+            {
+                bool sonuc = MukemmelSayiMi(i);
+                if (sonuc)
+                {
+                    Console.WriteLine("("+i+")"+" sayısı mükemmel sayıdır.");
+                }
+            }
+        }
+      
+    }
+}
---
+++ MukemmelSayiMi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MukemmelSayiMi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methodlarim m = new Methodlarim();
+            // m.PozitifBolenlertoplami(20);
+            // m.MukemmelSayiMi(28);
+            m.AraliktakiMukemmelSayilariBul(1, 10000);
+           
+            Console.ReadLine();
+        }
+    }
+}
